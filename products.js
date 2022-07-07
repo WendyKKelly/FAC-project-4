97499d99710680c3de6de4b67fa5381a1e471b81
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-07-07 13:47:42 -0700

    try this way

---
+++ products.js
@@ -0,0 +1,45 @@
+/*
+
+PRODUCTS
+
+*/
+
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+    123: {
+      name : "Banana",
+      desc : "banana.",
+      img : "assets/banana.jpg",
+      price : 2
+    },
+    124: {
+      name : "Apple",
+      desc : "apple.",
+      img : "assets/apple.jpg",
+      price : 1
+    },
+    125: {
+      name : "Mango",
+      desc : "mango.",
+      img : "assets/mango.jpg",
+      price : 6
+    },
+    126: {
+      name : "Orange",
+      desc : "orange. ",
+      img : "assets/orange.jpg",
+      price : 8
+    },
+    127: {
+        name : "Guava",
+        desc : "guava.",
+        img : "assets/guava.jpg",
+        price : 5
+      },
+      128: {
+        name : "Melon",
+        desc : "melon. ",
+        img : "assets/melon.jpg",
+        price : 842
+      }
+  };
